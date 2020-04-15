3210f79e0d2470a77f89948f6d73ca571d185e3c
Nieuwburg,Patrick P.E.J. van <p.vannieuwburg@student.fontys.nl>
2020-04-15 21:17:49 +0200

    initial commit
    initial commit

---
+++ Threading Counters/Counter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading_Counters
+{
+    public class Counter
+    {
+        public static void Count()
+        {
+            for (var number = 0; number < 100; number++)
+            {
+                Console.WriteLine($"{number} is counted by {Thread.CurrentThread.Name} ");
+            }
+        }
+    }
+}
---
+++ Threading Counters/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading_Counters
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            /*
+            NOTE TO SELF:
+            creating threads and initialize them, This didn't work, could not
+            start the threads so i need to initialize the name later.
+            var counter1 = new Thread(Counter.Count);
+            var counter2 = new Thread(Counter.Count);
+            var counter3 = new Thread(Counter.Count);
+            var counter4 = new Thread(Counter.Count);
+            var counter5 = new Thread(Counter.Count);
+            */
+
+            //creating threads and initialize them
+            var counter1 = new Thread(Counter.Count);
+            var counter2 = new Thread(Counter.Count);
+            var counter3 = new Thread(Counter.Count);
+            var counter4 = new Thread(Counter.Count);
+            var counter5 = new Thread(Counter.Count);
+
+            //Giving the counters a name to use in the .count() function
+            counter1.Name = "counter 1";
+            counter2.Name = "counter 2";
+            counter3.Name = "counter 3";
+            counter4.Name = "counter 4";
+            counter5.Name = "counter 5";
+
+            //Starting counters
+            counter1.Start();
+            counter2.Start();
+            counter3.Start();
+            counter4.Start();
+            counter5.Start();
+        }
+    }
+}
